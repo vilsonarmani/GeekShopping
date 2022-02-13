3b7a33230ff316e6b487d35d343c66e5bfc4d166
Vilson Pereira <Vilson.Pereira@ao3tech.com>
2022-02-13 19:46:37 -0300

    Criada a estrutura do Microserviços para Produto

---
+++ GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,36 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var connection = builder.Configuration["MySqlConnector:MySqlConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>( options => options.
+    UseMySql(connection, 
+            new MySqlServerVersion(new Version(5, 7, 11))));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
