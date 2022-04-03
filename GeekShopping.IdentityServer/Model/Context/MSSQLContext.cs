776f96b897532893b022b650748e5668eaffd9b2
Vilson Pereira <Vilson.Pereira@ao3tech.com>
2022-04-03 20:05:24 -0300

    Addict DbContext to Identity Server

---
+++ GeekShopping.IdentityServer/Model/Context/MSSQLContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.IdentityServer.Model.Context;
+
+public class MSSQLContext : IdentityDbContext<ApplicationUser>
+{
+    public MSSQLContext(DbContextOptions<MSSQLContext> options)
+        : base(options) { }
+}
+
